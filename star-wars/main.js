42eeddfe3b623e5d2d7575b2030f7f1ee5abf50f
Serena Draney <10848034@my.uvu.edu>
2020-10-23 12:48:02 -0600

    Added CSS and images with JS

--- star-wars/main.js
+++ star-wars/main.js
@@ -1,12 +1,30 @@
 import { films } from './data/films.js'
 import { people } from './data/people.js'
 
-//console.log(people.length)
+const main = document.querySelector('main')
 
-//console.log(films[0])
+for (let step = 0; step , 7; step++) {
+    let figure = document.createElement ('figure')
+    let figImg = document.createElement ('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[step].title
 
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
 
-people.forEach(person => {
+    main.appendChild(figure)
+}
+
+/* for (const film of films) {
+    let newImg = document.createElement('img')
+    newImg.src = 'https://starwars-visualguide.com/assets/img/characters/2.jpg'
+    main.appendChild(newImg)
+    console.log(film.title)
+} */
+
+/*people.forEach(person => {
     let newParagraph = document.body.appendChild(document.createElement('p'))
     newParagraph.textContent = person.name
-})
+}) */
+//http://starwars-visualguide.com/assets/log/characters/2.jpg
