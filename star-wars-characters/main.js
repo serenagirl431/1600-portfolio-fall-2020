998d30f848c534beafc25ffcf8bb86208d1f80ed
Serena Draney <10848034@my.uvu.edu>
2020-11-09 20:46:00 -0700

    added event listener

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -2,7 +2,7 @@ import { people } from '../data/people.js'
  
 const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
-document.body.appendChild(mainHeader)
+document.body.insertBefore(mainHeader, mainContent)
 
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Guys'
@@ -15,3 +15,9 @@ mainHeader.appendChild(femaleButton)
 const otherButton = document.createElement('button')
 otherButton.textContent = 'Non-Binary Pals'
 mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+maleButton.addEventListener('click', event => {
+    console.log(event.screenX, event.screenY)
+}
+)
