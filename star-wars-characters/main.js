5c0aaf6bd5eb351cec0a3f7e6030b3dbafcbcbea
Serena Draney <10848034@my.uvu.edu>
2020-11-10 11:30:54 -0700

    Done with class 10/27!

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -1,8 +1,10 @@
 import { people } from '../data/people.js'
+
+const mainContent = document.querySelector('#main')
  
 const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
-document.body.insertBefore(mainHeader, mainContent)
+document.body.insertBefore (mainHeader, mainContent)
 
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Guys'
@@ -17,7 +19,26 @@ otherButton.textContent = 'Non-Binary Pals'
 mainHeader.appendChild(otherButton)
 
 const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters)
+
 maleButton.addEventListener('click', event => {
-    console.log(event.screenX, event.screenY)
+maleCharacters.forEach((element) => {
+   const charFigure = document.createElement('figure')
+   const charImg = document.createElement('img')
+   charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+   const charCaption = document.createElement('figcaption')
+   charCaption.textContent = `Yummy, Yummy`
+
+   charFigure.appendChild(charImg)
+   charFigure.appendChild(charCaption)
+
+   mainContent.appendChild(charFigure)
+} )
+} )
+
+let theUrl = "https://swapi.co/api/people/2/"
+
+function getLastNumber(url) {
+    console.log(url)
 }
-)
+getLastNumber(theUrl)
