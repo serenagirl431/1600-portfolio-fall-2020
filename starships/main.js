c0083b2d94a69a6264157363c8f5e3b4de12dfe9
Serena Draney <10848034@my.uvu.edu>
2020-11-15 00:31:02 -0700

    added framework and modal

--- starships/main.js
+++ starships/main.js
@@ -5,6 +5,15 @@ const shipView = document.querySelector('.main')
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+modalBackground.addEventListener('click', () => {
+        dialog.classList.toggle("is-active")
+})
 function populateNav(starships) {
     starships.forEach(starship => {
         let anchorWrap = document.createElement('a')
@@ -24,9 +33,13 @@ function populateNav(starships) {
 }
 function populateShipView(shipData) {
     removeChildren(shipView)
-    let shipImage =document.createElement('img')
+    let shipImage = document.createElement('img')
     let shipNum = getLastNumber(shipData.url)
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        dialog.classList.toggle("is-active")
+    })
     shipView.appendChild(shipImage)
 }
 
