1972393b6dedfb36af016671b962d0860cfb2481
Serena Draney <10848034@my.uvu.edu>
2020-11-17 22:06:42 -0700

    added listener event stuff

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -3,10 +3,11 @@ import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('main')
 
-for (let step = 0; step , 7; step++) {
+for (let step = 0; step < 7; step++) {
     let figure = document.createElement ('figure')
     let figImg = document.createElement ('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
+    let poop = `asdfasdf${step}`
     let figCaption = document.createElement('figcaption')
     
     const foundFilm = films.find(film => getLastNumber(film.url) == (step + 1))
