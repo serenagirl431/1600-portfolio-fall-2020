6c3dfda2d5b4db0d21eb1f3f45793b6842416d49
Serena Draney <10848034@my.uvu.edu>
2020-11-23 22:09:46 -0700

    fixed figCaptions to  show up correctly

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -7,10 +7,9 @@ for (let step = 0; step < 7; step++) {
     let figure = document.createElement ('figure')
     let figImg = document.createElement ('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
-    let poop = `asdfasdf${step}`
     let figCaption = document.createElement('figcaption')
     
-    const foundFilm = films.find(film => getLastNumber(film.url) == (step + 1))
+    const foundFilm = films.find(film => parseInt(getLastNumber(film.url)) === (step + 1))
     figCaption.textContent = foundFilm.title
 
     figure.appendChild(figImg)
